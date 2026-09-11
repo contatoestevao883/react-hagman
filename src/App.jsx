3b18c0b15468aeb8b48e1b558d6ab14022c1063b
contatoestevao883 <contatoestevao883@hotmail.com>
2026-09-11 16:30:30 -0300

    feat: wire App with GameProvider, Jogo and Letras

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { styled } from 'styled-components'
+import Jogo from './components/Jogo/Jogo'
+import Letras from './components/Letras/Letras'
+import GameProvider from './context/GameContext'
+
 function App() {
   return (
-    <div>
-      <h1>Jogo da Forca</h1>
-    </div>
+    <GameProvider>
+      <DivContainer>
+        <Jogo />
+        <Letras />
+      </DivContainer>
+    </GameProvider>
   )
 }
 
 export default App
+
+const DivContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
